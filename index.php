78eefcae6a6642c548289a23f8b451c5ed1c120d
Erin Brunken <erin@flyingfox.net.nz>
2017-12-09 22:14:23 +0000

    Created the nav and added a background image to the body.

--- index.php
+++ index.php
@@ -4,25 +4,42 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Bootstrap 101 Template</title>
-
-    <!-- Bootstrap -->
+    <title>Online Notes</title>
     <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
   </head>
   <body>
-    <h1>Hello, world!</h1>
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+       <!--Nav Bar-->
+       <nav role="navigation" class="navbar navbar-fixed-top navbar-custom">
+           <div class="container-fluid">
+               <div class="navbar-header">
+                   <a href="" class="navbar-brand">Online Notes</a>
+                   <button type="button" class="navbar-toggle" data-target="#navbar-collapse" data-toggle="collapse">
+                       <span class="sr-only">Toggle navigation</span>
+                       <span class="icon-bar"></span>
+                       <span class="icon-bar"></span>
+                       <span class="icon-bar"></span>
+                   </button>
+               </div>
+               <div class="navbar-collapse collapse" id="navbar-collapse">
+                   <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="#">Help</a></li>
+                        <li><a href="#">Contact</a></li>
+                   </ul>
+                   <ul class="nav navbar-nav navbar-right">
+                        <li class=""><a href="#">Login</a></li>
+                   </ul>
+               </div>
+           </div>
+       </nav>
+   
+   
+   
+   
+   
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
   </body>
 </html>
